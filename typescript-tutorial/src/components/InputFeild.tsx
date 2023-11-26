d8a60650314d59eb9decfa2e855c1b8de35157ab
BrunoBReis <brunobragancadosreis@gmail.com>
2023-11-26 19:41:13 -0300

    implementing another part of code

--- typescript-tutorial/src/components/InputFeild.tsx
+++ typescript-tutorial/src/components/InputFeild.tsx
@@ -1,1 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react'
+import "./styles.css"
+// rafce helps to creat an simple struct
+
+// Props has all todo types
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
+}
+
+// const InputFeild: React.FC<props> = ... works the same
+// InputFeils has todo and setTodo 
+const InputFeild = ({ todo, setTodo, handleAdd }: Props) => {
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    // this event (onSubmit) solves the blur when press enter
+    return (
+        <form className='input' onSubmit={(e) => {
+            handleAdd(e);
+            inputRef.current?.blur();
+            }}>
+            <input
+                ref={inputRef}
+                type='input'
+                value={todo}
+                onChange={(e) => setTodo(e.target.value)}
+                placeholder='Enter a tesk'
+                className='input__box'></input>
+            <button className='input_submit' type='submit'>
+                Go
+            </button>
+        </form>
+    )
+}
+
+export default InputFeild
---
+++ typescript-tutorial/src/model.ts
@@ -0,0 +1,6 @@
+// export beacause it needs to export
+export interface Todo{
+    id: number;
+    todo: string;
+    isDone: boolean;
+}
